075ebc4149b7aab38379c3e9c26a0e5dbea56195
SM RIYAD <smriyad0185@gmail.com>
2023-10-17 21:47:01 +0600

    connected mongodb

--- index.js
+++ index.js
@@ -1,16 +1,67 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
+
 require('dotenv').config();
 const port = process.env.PORT || 5000;
-
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 // middleware
 app.use(cors());
 app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.wacbf1n.mongodb.net/?retryWrites=true&w=majority`;
+
+// mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.wacbf1n.mongodb.net/?retryWrites=true&w=majority
+console.log(uri);
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+async function run() {
+    try {
+        // Connect the client to the server	(optional starting in v4.7)
+        await client.connect();
+
+        const brandCollection = client.db('brandWebsite').collection('brandNames');
+      
+
+        ///getting brand names api
+        app.get('/brands', async (req, res) => {
+            const cursor = brandCollection.find();
+            const result = await cursor.toArray();
+            res.send(result);
+        })
+
+       
+       
+
+       
+     
+
+       
+
+      
+
+        // Send a ping to confirm a successful connection
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // Ensures that the client will close when you finish/error
+        // await client.close();
+    }
+}
+run().catch(console.dir);
+
 app.get('/', (req, res) => {
     res.send('brand server is running')
 })
 
 app.listen(port, () => {
-    console.log(`brand Server is running on port: ${port}`)
+    console.log(`brand Server is running on port: ${port}, ${process.env.DB_USER},${process.env.DB_PASS} ` )
 })
